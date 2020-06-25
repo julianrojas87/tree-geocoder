b589aa4c737e739438119f1befdf24c38cd85105
Julian Rojas <julian.rojas87@gmail.com>
2020-06-25 11:57:01 +0200

    Added config file and set sources back to public IPs

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    source: {
+        prefix: 'http://193.190.127.152/geonames-prefix-tree/node0.jsonld#Collection',
+        suffix: 'http://193.190.127.152/geonames-suffix-tree/node0.jsonld#Collection'
+    }
+};
